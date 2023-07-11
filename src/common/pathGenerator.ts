3fd3e6ab619fc587aa7cd36ad455b7202de728f0
ShamalRathnayake <shamalkavinda44@gmail.com>
2023-07-11 22:50:59 +0530

    added path generation changes

--- src/common/pathGenerator.ts
+++ src/common/pathGenerator.ts
@@ -8,23 +8,62 @@ import { join } from 'path';
 
 export default class PathGenerator {
   private static backupPath: string = '';
+  private static backupDirectory: string = '';
+  private static oldBackupPath: string = '';
+  private static oldBackupDirectory: string = '';
   private constructor() {}
 
-  public static generatePaths(options: BackupOptions) {
+  public static generatePaths(options: BackupOptions): {
+    backupPath: string;
+    backupDirectory: string;
+    oldBackupPath: string;
+    oldBackupDirectory: string;
+  } {
     BackupLogger.log(logConfig.types.debug, `Generating paths`);
     if (!options.out && !options.archive) {
-      this.backupPath = this.dirCheck(config.defaultBackupPath);
+      this.backupPath = this.dirCheck(
+        join(config.defaultBackupPath, TimestampGenerator.generateTimestamp())
+      ).trim();
+      this.backupDirectory = this.dirCheck(config.defaultBackupPath).trim();
       BackupLogger.log(
         logConfig.types.debug,
-        `Using default backup path - "${config.defaultBackupPath}"`
+        `Using default backup directory - "${config.defaultBackupPath}"`
       );
     } else if (options.out) {
-      this.backupPath = this.dirCheck(join(options.out, TimestampGenerator.generateTimestamp()));
+      this.backupPath = this.dirCheck(
+        join(options.out, TimestampGenerator.generateTimestamp())
+      ).trim();
+      this.backupDirectory = this.dirCheck(options.out).trim();
     } else if (options.archive) {
       this.backupPath = this.dirCheck(
         join(options.archive, TimestampGenerator.generateTimestamp())
-      );
+      ).trim();
+      this.backupDirectory = this.dirCheck(options.archive).trim();
     }
+
+    if (options.removeOldBackups) {
+      this.oldBackupDirectory = join(
+        this.backupDirectory,
+        TimestampGenerator.generateRangeTimestamp(
+          false,
+          false,
+          options.localBackupRange || undefined
+        ).trim()
+      );
+
+      this.oldBackupPath = `${this.oldBackupDirectory}/${TimestampGenerator.generateRangeTimestamp(
+        true,
+        false,
+        options.localBackupRange || undefined
+      )}`.trim();
+    }
+
+    return {
+      backupPath: this.backupPath,
+      backupDirectory: this.backupDirectory,
+      oldBackupPath: this.oldBackupPath,
+      oldBackupDirectory: this.oldBackupDirectory,
+    };
   }
 
   private static dirCheck(path: string): string {
--- src/common/timestampGenerator.ts
+++ src/common/timestampGenerator.ts
@@ -1,11 +1,13 @@
+import deleteConfig from '../backup/config/deleteConfig';
+
 export default class TimestampGenerator {
   private static date = new Date();
 
   private constructor() {}
 
-  public static generateTimestamp(withSeconds: boolean = false, newDate: boolean = false) {
+  public static generateTimestamp(time: boolean = false, newDate: boolean = false) {
     if (newDate) this.date = new Date();
-    if (withSeconds) {
+    if (time) {
       return `${`0${new Date(this.date).getHours()}`.slice(-2)}H_${`0${new Date(
         this.date
       ).getMinutes()}`.slice(-2)}M_${`0${new Date(this.date).getSeconds()}`.slice(-2)}S`;
@@ -13,6 +15,27 @@ export default class TimestampGenerator {
       return `${new Date(this.date).getFullYear()}_${new Date(this.date).getMonth() + 1}_${new Date(
         this.date
       ).getDate()}`;
+    }
+  }
+
+  public static generateRangeTimestamp(
+    time: boolean = false,
+    newDate: boolean = false,
+    range: number = deleteConfig.localBackupRange.default
+  ) {
+    if (newDate) this.date = new Date();
+
+    const date = new Date(this.date);
+    const previousBackupDate = new Date(date.setDate(date.getDate() - range));
+
+    if (time) {
+      return `${`0${new Date(previousBackupDate).getHours()}`.slice(-2)}H_${`0${new Date(
+        previousBackupDate
+      ).getMinutes()}`.slice(-2)}M_${`0${new Date(previousBackupDate).getSeconds()}`.slice(-2)}S`;
+    } else {
+      return `${new Date(previousBackupDate).getFullYear()}_${
+        new Date(previousBackupDate).getMonth() + 1
+      }_${new Date(previousBackupDate).getDate()}`;
     }
   }
 }
